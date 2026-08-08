d9c131a1f1816ad45ce655b346f6a83a60102147
Anand verma <anandvermaverma9009@gmail.com>
2026-08-08 23:11:54 +0530

    Add testbench for NAND gate

---
+++ 02_Basic_Gates/nand_gate_tb.v
@@ -0,0 +1,28 @@
+// Code your testbench here
+// or browse Examples
+module nand_gate_tb;
+reg a, b;
+wire y;
+nand_gate uut (.a(a), .b(b), .y(y));
+
+initial begin
+
+    $monitor("Time=%0t | a=%b b=%b | y=%b", $time, a, b, y);
+  $dumpfile("nand_gate.vcd");
+  $dumpvars(0, nand_gate_tb);
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+end
+
+endmodule
